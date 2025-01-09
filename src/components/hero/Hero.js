61513ee82d822bda3e6fed615b592bcc0e653cfc
Damian Alvarez <dimeinc2@gmail.com>
2025-01-08 21:17:34 -0600

    First commit

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,34 @@
+import './Hero.css';``
+import  Carousel  from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
+
+const Hero = ({movies}) => {
+    return(
+        <div  className="movie-carousel-container">
+            <Carousel>
+                {
+                    movies?.map((movie) => {
+                        return(
+                            <Paper key={movie.imdbId}>
+                                <div className="movie-card-container">
+                                    <div className="movie-card">
+                                        <div className="movie-detail">
+                                        <div className="movie-card-image">
+                                            <img src={movie.poster} alt={movie.title} />
+                                        </div>
+                                        <div className="movie-title">
+                                            <h2>{movie.title}</h2>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Paper>
+                        )
+                    })
+                }
+            </Carousel>
+        </div>
+    )
+}
+
+export default Hero;
